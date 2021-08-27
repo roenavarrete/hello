57be0f89902e8d609efa9d0b351f0ce8ed818ad3
Nicholas <nwitulski2@huskers.unl.edu>
2021-08-27 01:23:18 +0000

    Nic change

--- hello.c
+++ hello.c
@@ -5,4 +5,8 @@ int main(int argc, char **argv) {
 
 printf("Roel Navarrete!\n");
 printf("Computer Science\n");
+
+// This is Nicholas's lines of code://
+  printf("\nNicholas Witulski\n");
+  printf("Electrical Engineering\n");
 }
